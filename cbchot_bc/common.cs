e754ec0628910fbaa22312ab0c28b9377f9342b1
kurt7 <huicai1980@gmail.com>
2020-03-16 17:31:53 +0800

    x

---
+++ cbchot_bc/common.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cbchot_bc
+{
+    class common
+    {
+    }
+
+    class owner
+    {
+        string owner_name;
+        string owner_id;
+        ArrayList video_list;
+    }
+
+    class video
+    {
+        string video_name;
+        string video_describe;
+        string video_id;
+        string video_etag;
+        int length;
+
+    }
+
+    class play_record
+    {
+        string client_id;
+        string v_id;
+        string start_dt;
+        string stop_dt;
+    }
+
+
+}
